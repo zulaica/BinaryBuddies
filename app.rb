c630c36d46af4669405787d234cab9415ece4492
David Zulaica <zulaica@gmail.com>
2015-02-02 16:43:08 -0800

    Set up BinaryBuddy creation in Sinatra.

--- app.rb
+++ app.rb
@@ -4,5 +4,12 @@ Bundler.require :default
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 
 get "/" do
+  @buddies = BinaryBuddy.all
   erb(:index)
+end
+
+post "/buddies" do
+  buddy_name = params.fetch("name")
+  BinaryBuddy.create(name: buddy_name)
+  redirect "/"
 end
